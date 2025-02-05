a30688fb6383aeded11af6305d762ebadef4bfeb
Avishkar-1505 <avishkarrl23087@gmail.com>
2025-02-05 22:47:29 +0530

    Added functionality to buttons. Changed Grid drawing logic to suit reset and custome size buttons

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ container.appendChild(grid);
 
 grid.classList = "grid"
 
-let defaultSize = 16;
+const defaultSize = 16;
 const gridDim = 680;
+
+const colors = ["#DAF7A6", "#FFC300", "#FF5733", "#C70039", "#900C3F", "#581845"]
+
+let customeSize = 0;
 
 function drawGrid(size) {
     const curDim = gridDim/size;
@@ -16,23 +20,55 @@ function drawGrid(size) {
         square.classList = "square"
         square.style.cssText = `width: ${curDim}px; height: ${curDim}px;`;
         grid.appendChild(square);
+        square.addEventListener("mouseover", ()=>{
+            const color = colors[(Math.floor(Math.random()*10))%6];
+            square.style.background = `${color}`;
+        })
     }
 
 }
 
 drawGrid(defaultSize);
 
-const colors = ["#DAF7A6", "#FFC300", "#FF5733", "#C70039", "#900C3F", "#581845"]
 
 
-const squares = document.querySelectorAll(".square");
 
-squares.forEach((square) => {
-    square.addEventListener("mouseover", ()=>{
-        const color = colors[(Math.floor(Math.random()*10))%6];
-        square.style.background = `${color}`;
+// const squares = document.querySelectorAll(".square");
+
+// squares.forEach((square) => {
+//     square.addEventListener("mouseover", ()=>{
+//         const color = colors[(Math.floor(Math.random()*10))%6];
+//         square.style.background = `${color}`;
+//     })
+// })
+
+
+function resetGrid(size) {
+    const squares = document.querySelectorAll(".square")
+    squares.forEach((square)=>{
+        square.remove();
     })
+    drawGrid(size);
+}
+
+
+const resetBtn = document.querySelector("#reset");
+const custSz = document.querySelector("#customSz");
+
+resetBtn.addEventListener("click", ()=>{
+    if(customeSize!=0){
+        resetGrid(customeSize);
+    }
+    else{
+        resetGrid(defaultSize);
+    }
 })
 
+custSz.addEventListener("click", ()=>{
+    customeSize = Number(prompt("Enter Grid Size (Less than 100 units)"));
+    if(customeSize > 100){
+        customeSize = 100;
+    }
 
-
+    resetGrid(customeSize);
+})
